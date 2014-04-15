f97d29cc43d5e4c0c63225babfd02a7bb8476581
Lance Ramoth <lance.ramoth@gmail.com>
2014-04-15 10:05:55 -0700

    removed CORS logic to bins controller

--- app/controllers/api/bins_controller.rb
+++ app/controllers/api/bins_controller.rb
@@ -1,7 +1,7 @@
 class API::BinsController < ApplicationController
  
   skip_before_filter  :verify_authenticity_token
-  after_filter :cors_set_access_control_headers
+  #after_filter :cors_set_access_control_headers
 
   # GET /bins
   def index
@@ -47,11 +47,11 @@ class API::BinsController < ApplicationController
   end
 
   # CORS preflight handled at nginx level
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
+  #def cors_set_access_control_headers
+    #headers['Access-Control-Allow-Origin'] = '*'
+    #headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    #headers['Access-Control-Max-Age'] = "1728000"
+  #end
 
   private
     def bin_params
