f077ba7a24770a3196c19587eff4a6dce00621f3
Lance Ramoth <lance.ramoth@gmail.com>
2014-04-21 22:05:49 -0700

    copy change

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -2,7 +2,7 @@ class API::HomeController < ApplicationController
   
   def index
     respond_to do |format|
-      format.text { render :text => "Welcome to the myJSON api." }
+      format.text { render :text => "Welcome to the myjson api." }
     end
   end
 
