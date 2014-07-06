d86ec24ede59f210be9b0baadde80443a51577c6
lramoth <lance.ramoth@gmail.com>
2014-07-06 16:41:34 -0700

    removed turbolinks per http://blog.steveklabnik.com/posts/2013-06-25-removing-turbolinks-from-rails-4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  #before_filter :set_no_cache
+  #def set_no_cache
+  #  response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+  #  response.headers["Pragma"] = "no-cache"
+  #  response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  #end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
 end
